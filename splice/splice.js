61eb55059713fd8d94607ac809457ef43a54fc1e
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-01 19:37:13 +0200

    feat(removeReplace): Add constant
    * Add removeReplace()
    * Demonstrate removing first 3 elements, and replacing them.

--- splice/splice.js
+++ splice/splice.js
@@ -1,14 +1,29 @@
 "use strict";
 
-// Ex: 1 - splice() - Remove one item ------------
+// Ex: 1 - splice() - Remove one item ------------------------------------------
 let arr = [ "I", "study", "JavaScript" ];
 
 const removeElement = () => {
     console.group("removeElement()");
 
     console.log("Original Array: " + arr);
-
     arr.splice(1, 1);
     console.log("Modified Array: " + arr);
+
+    console.groupEnd();
 }
 removeElement();
+
+// Ex: 2 - splice() - Remove and Replace ---------------------------------------
+let arr_ex_2 = ["I", "study", "JavaScript", "right", "now"];
+
+const removeReplace = () => {
+    console.group("removeReplace");
+
+    console.log("Original Array: " + arr_ex_2);
+    arr_ex_2.splice(0, 3, "Let's", "dance");
+    console.log("Modified Array: " + arr_ex_2)
+
+    console.groupEnd();
+}
+removeReplace();
