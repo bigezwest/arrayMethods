cf5cc205091959c3ab46afbe0d00382a2b3ca1e0
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-01 18:32:52 +0200

    feature(removeElement): Add const
    * This removes one element from an array using splice().

--- splice/splice.js
+++ splice/splice.js
@@ -1,1 +1,14 @@
-"use strict";ß
+"use strict";
+
+// Ex: 1 - splice() - Remove one item ------------
+let arr = [ "I", "study", "JavaScript" ];
+
+const removeElement = () => {
+    console.group("removeElement()");
+
+    console.log("Original Array: " + arr);
+
+    arr.splice(1, 1);
+    console.log("Modified Array: " + arr);
+}
+removeElement();
