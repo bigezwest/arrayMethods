be9801b4189967e5921da33f44e2823f2d657dea
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-02 18:41:50 +0200

    feature(includes.js): Add Example 1

--- includes/includes.js
+++ includes/includes.js
@@ -1,1 +1,8 @@
 "use strict";
+let arr = [ 1, 0, false ];
+
+console.group("includes");
+console.log("Array: " + arr);
+console.log("includes(1): " + arr.includes(1));
+console.log("arr.includes(Nan): " + arr.includes(NaN));
+console.groupEnd();
