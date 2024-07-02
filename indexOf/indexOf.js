ade648c2a504afdb98f7aa18a75aa19e649bf948
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-02 17:46:53 +0200

    docs(indexOf()): Modify console.log statement

--- indexOf/indexOf.js
+++ indexOf/indexOf.js
@@ -1,8 +1,8 @@
 "use strict";
 
 let arr = [ 1, 0, false ];
-
-console.group("indexOf");
+// Ex: 1 - indexOf - item only -------------------
+console.group("Ex: 1 - indexOf");
 console.log("Array: " + arr);
 console.log("indexOf(0): " + arr.indexOf(0));
 console.log("indexOf(1): " + arr.indexOf(1));
@@ -10,3 +10,4 @@ console.log("indexOf(false): " + arr.indexOf(false));
 
 console.log("indexOf(null): " + arr.indexOf(null));
 console.log("indexOf(NaN): " + arr.indexOf(NaN));
+console.groupEnd();
