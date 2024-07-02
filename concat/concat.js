f76f11668107bb72f6cbbd94eb74e26a7dc108a5
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-02 15:34:04 +0200

    feature(concat()); Concat() 2 arrays

--- concat/concat.js
+++ concat/concat.js
@@ -1,7 +1,15 @@
 "use strict";
 
+// Initial Array ---------------------------------
 let arr = [ 1, 2 ];
 
 console.group("Original Array");
 console.log(arr);
+console.groupEnd();
+
+// concat() - with another array -----------------
+const arr_2 = arr.concat([3, 4]);
+
+console.group("Concat 2 arrays");
+console.log(arr_2);
 console.groupEnd();
