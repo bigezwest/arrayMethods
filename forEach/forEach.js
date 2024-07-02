7f730a16c2713dc1751fc1ac36b3ecd00fbf1a28
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-02 16:55:01 +0200

    feature(forEach): Ex 1 - show each item
    * Use the item parameter with foreach and console log each item.

--- forEach/forEach.js
+++ forEach/forEach.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// Ex: 1 - foreach - console log each item -------
+const arr = [ "Bilbo", "Gandolf", "Nazgul" ];
+console.group("forEach item");
+
+arr.forEach((item) => {
+    console.log(item);
+});
+
+console.groupEnd();
