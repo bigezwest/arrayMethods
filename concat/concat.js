1e19cd4a1604d77c74c6682c8717a853169c9144
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-02 15:49:48 +0200

    feature(ex_3_arr); Add ex
    * This shows concat one array with another and also 2 single values.

--- concat/concat.js
+++ concat/concat.js
@@ -1,21 +1,27 @@
 "use strict";
 
-// Initial Array ---------------------------------
+// Initial Array ---------------------------------------------------------------
 let arr = [ 1, 2 ];
 
 console.group("Original Array");
 console.log(arr);
 console.groupEnd();
 
-// Ex: 1 - concat() - with another array ---------
+// Ex: 1 - concat() - with another array ---------------------------------------
 const ex_1_arr = arr.concat([3, 4]);
 
 console.group("Ex 1: Concat 2 arrays");
 console.log(ex_1_arr);
 console.groupEnd();
 
-// Ex: 2 - concat - one array with 2 arg arrays --
+// Ex: 2 - concat - one array with 2 arg arrays --------------------------------
 const ex_2_arr = arr.concat([3, 4], [5, 6]);
 console.group("Ex 2: Concat with 2 arg arrays");
 console.log(ex_2_arr);
+console.groupEnd();
+
+// Ex: 3 - concat - one array with another and then single values --------------
+const ex_3_arr = arr.concat([3, 4], 5, 6);
+console.group("Ex 3: Concat with array and 2 single values");
+console.log(ex_3_arr);
 console.groupEnd();
