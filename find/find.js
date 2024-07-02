f4f1a293b1b357afb5eb8b31d4f31f0b269604d5
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-02 19:20:49 +0200

    feature(results): show results as console.logs
    * Show individual user data in console log statements for match

--- find/find.js
+++ find/find.js
@@ -9,3 +9,8 @@ let user = users.find(item => item.id == 1);
 console.group("Table");
 console.table(user);
 console.groupEnd();
+
+console.group("Key Values");
+console.log("Id: " + user.id);
+console.log("Name: " + user.name);
+console.groupEnd();
