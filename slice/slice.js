99772373c3b6dd5c245508471fd5886f28891a3a
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-02 10:26:25 +0200

    feat(slice()): Add with start and end
    * Add original array
    * Add a slice(start, end) example

--- slice/slice.js
+++ slice/slice.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let arr = [ "t", "e", "s", "t" ];
+
+// Ex: 1 - Original array ---------------------------------------
+console.group("Original Array");
+console.log(arr);
+console.groupEnd();
+
+const arrSlice = (start, end) => {
+    return (arr.slice(start, end));
+}
+
+// Sliced array ----------------------------------
+console.group("Ex 1 - Array Slice");
+console.log(arrSlice(1, 3));
+console.groupEnd();
+
+// Original array --------------------------------
+console.group("Original Array");
+console.log(arr);
+console.groupEnd();
