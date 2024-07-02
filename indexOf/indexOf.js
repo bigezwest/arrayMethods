affd32dfde9754fadf5bb1eb72b9f4e2e9bc4059
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-02 17:44:45 +0200

    feature(indexOf()): Add examples using item only

--- indexOf/indexOf.js
+++ indexOf/indexOf.js
@@ -1,1 +1,12 @@
 "use strict";
+
+let arr = [ 1, 0, false ];
+
+console.group("indexOf");
+console.log("Array: " + arr);
+console.log("indexOf(0): " + arr.indexOf(0));
+console.log("indexOf(1): " + arr.indexOf(1));
+console.log("indexOf(false): " + arr.indexOf(false));
+
+console.log("indexOf(null): " + arr.indexOf(null));
+console.log("indexOf(NaN): " + arr.indexOf(NaN));
