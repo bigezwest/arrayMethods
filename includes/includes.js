c0687b68c31561dcbf8bfd2c5ab6d1c9fa7acd9d
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-02 18:42:37 +0200

    feature(includes.js): Add array[NaN]
    * Show that includes will also work for NaN

--- includes/includes.js
+++ includes/includes.js
@@ -6,3 +6,9 @@ console.log("Array: " + arr);
 console.log("includes(1): " + arr.includes(1));
 console.log("arr.includes(Nan): " + arr.includes(NaN));
 console.groupEnd();
+
+let arr_2 = [NaN];
+console.group("includes");
+console.log(arr_2);
+console.log("arr_2.includes(NaN): " + arr_2.includes(NaN));
+console.groupEnd();
