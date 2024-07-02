5b1db01133ab06f28d2272cd6d97e32080a1af1c
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-02 16:58:57 +0200

    feature(forEach): Ex 2 - use all 3 paramters
    * Console log all 3 paramters (item, index, array), for each iteration
    of the foreach loop.

--- forEach/forEach.js
+++ forEach/forEach.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Ex: 1 - foreach - console log each item -------
+// Ex: 1 - foreach - console log each item -------------------------------------
 const arr = [ "Bilbo", "Gandolf", "Nazgul" ];
 console.group("forEach item");
 
@@ -8,4 +8,12 @@ arr.forEach((item) => {
     console.log(item);
 });
 
+console.groupEnd();
+
+// Ex: 2 - foreach - use all 3 parameters of foreach(item, index, array) &nbsp -
+console.group("forEach - item, index, array");
+
+arr.forEach((item, index, array) => {
+    console.log(`Item: ${item}, Index: ${index}, in ${array}`);
+});
 console.groupEnd();
