0fa6b7e29e025354c1f9badb50267a01f223b106
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-03 10:31:26 +0200

    feature(sort num_arr):
    * This is to show that the array is sorted as strings.

--- sort/sort.js
+++ sort/sort.js
@@ -7,3 +7,9 @@ console.log("Unsorted: " + char_arr);
 
 char_arr.sort();
 console.log("Sorted: " + char_arr);
+
+// Ex: 2 - sort - numbers - sorts as a string
+let num_arr = [ 2, 15, 3, 1, 4 ];
+console.log("Unsorted: " + num_arr);
+num_arr.sort();
+console.log("Sorted: " + num_arr);
