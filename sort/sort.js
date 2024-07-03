7485fb82a9ec1a8390807f8039bab6a672c60d8e
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-03 10:26:39 +0200

    feature(sort characters): Add example
    * This is to show a successful sort.

--- sort/sort.js
+++ sort/sort.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// Ex: 1 - sort - single characters --------------
+let char_arr = [ "b", "c", "a", "f", "d" ];
+
+console.log("Unsorted: " + char_arr);
+
+char_arr.sort();
+console.log("Sorted: " + char_arr);
