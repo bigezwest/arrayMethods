e1997e9402ccc4b01e747e7ff4ca67ea1041c16c
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-03 17:05:04 +0200

    feature(spllit(delim)): Add Ex: 1
    * This example shows spit using only the delimiter parameter.

--- split/split.js
+++ split/split.js
@@ -1,1 +1,8 @@
 "use strict";
+
+// Ex: 1 - split - delim is only parameter -------------------------------------
+const text = "Tom, Dick, Harry" ;
+let names = text.split(',');
+for (let name of names) {
+    console.log(`A message to ${name}`);
+}
