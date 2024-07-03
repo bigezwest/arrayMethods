13d5a713a7662015e7b0e7bdf1f86d2745941b06
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-03 17:10:05 +0200

    chore(grouping): Add group and group ends

--- split/split.js
+++ split/split.js
@@ -1,15 +1,18 @@
 "use strict";
 
 // Ex: 1 - split - delim is only parameter -------------------------------------
+console.group("Ex: 1");
 const text = "Tom, Dick, Harry" ;
 let names = text.split(',');
 for (let name of names) {
     console.log(`A message to ${name}`);
 }
-
-// Ex: 2 - split - delim and limit paramters ----------------------
+console.groupEnd();
+// Ex: 2 - split - delim and limit parameters ----------------------------------
+console.group("Ex: 2");
 const text_2 = "Tom, Dick, Harry";
 let names_2 = text_2.split(", ", 2);
 for (let name of names_2) {
     console.log(`A message to ${name}`);
 }
+console.groupEnd();
