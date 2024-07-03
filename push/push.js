9193a6bd0bd819367ae00825c84e78079e50cb6e
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-03 18:27:35 +0200

    feature(Ex: 1): Add push
    * Create names array
    * Create newNames array
    * Push newNames to names

--- push/push.js
+++ push/push.js
@@ -1,1 +1,7 @@
 "use strict";
+
+let names = [ "Tom", "Dick", "Harry" ];
+let newNames = [ "Fred", "Barney" ];
+console.log("Original names array: " + names);
+names.push(...newNames);
+console.log("Names array after push: " + names);
