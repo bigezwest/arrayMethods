05d64b27eae782d876cd2b6f73deb8d8d902474e
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-03 18:20:12 +0200

    feature(isArray()): Add example
    * Show the use of isArray

--- isArray/isArray.js
+++ isArray/isArray.js
@@ -5,3 +5,9 @@ console.group("typeOf()");
 console.log("typeof {}: " + typeof {});
 console.log("typeof []: " + typeof []);
 console.groupEnd();
+
+// Ex: 2 - Show use of isArray -------------------------------------------------
+console.group("isArray()");
+console.log("isArray({}): " + Array.isArray({}));
+console.log("isArray([]): " + Array.isArray([]));
+console.groupEnd();
