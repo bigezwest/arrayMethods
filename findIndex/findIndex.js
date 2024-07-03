11c3eccd0dc64269b4fa5c475c602a21291a4ffd
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-03 03:55:50 +0200

    feature(findIndex): Add example
    * Find user John, using user.name

---
+++ findIndex/findIndex.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let users = [
+    {id: 1, name: "John"},
+    {id: 2, name: "Pete"},
+    {id: 3, name: "Mary"},
+    {id: 4, name: "John"}
+];
+
+// Ex: 1 - findIndex - ---------------------------------------------------------
+console.group("FindIndex: ");
+const userJohnIndex = users.findIndex(user => user.name == "John");
+console.log("John Index: " + userJohnIndex);
+console.groupEnd();
