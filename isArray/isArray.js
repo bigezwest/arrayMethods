4f17d655e8f90eb85694d755660aaf5e7104d79e
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-03 18:13:02 +0200

    feature(typeof()): Add typeof example
    * Add an example that shows typeof does not return an isArray type value
    It only returns that it is an object.

--- isArray/isArray.js
+++ isArray/isArray.js
@@ -1,1 +1,7 @@
 "use strict";
+
+// Ex: 1 - Show that typeof() does not work ------------------------------------
+console.group("typeOf()");
+console.log(typeof {});
+console.log(typeof []);
+console.groupEnd();
