0081d2646087094ed5ed15d0653e690b975ece8c
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-03 17:08:12 +0200

    feature(Ex: 2): Add example 2
    * This shows split using both the delimiter and limit parameters

--- split/split.js
+++ split/split.js
@@ -6,3 +6,10 @@ let names = text.split(',');
 for (let name of names) {
     console.log(`A message to ${name}`);
 }
+
+// Ex: 2 - split - delim and limit paramters ----------------------
+const text_2 = "Tom, Dick, Harry";
+let names_2 = text_2.split(", ", 2);
+for (let name of names_2) {
+    console.log(`A message to ${name}`);
+}
