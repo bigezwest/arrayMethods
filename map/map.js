5d432465d3ea5876a44c01a8fd9410eb38ecffac
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-03 10:17:42 +0200

    feature(names): Map array of strings
    * Map an array of strings, show length of each.

--- map/map.js
+++ map/map.js
@@ -1,1 +1,8 @@
 "use strict";
+
+// Ex: 1 - Map -----------------------------------
+const names = [ "Tom", "Dick", "Harry" ];
+let lengths = names.map(item => item.length);
+console.group("Map names");
+console.log(lengths);
+console.groupEnd();
