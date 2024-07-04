507ff06ce44b126d93ec8fed6d56361291b66856
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 14:20:05 +0200

    feature(upperFirstLetter): Add function
    * Add function to convert a string's first letter to an uppercase.
    * Return the string

--- tasks/translate/translate.js
+++ tasks/translate/translate.js
@@ -5,6 +5,14 @@ let arr = input.split("-");
 for (let word = 0; word < arr.length; word++) {
     if (word > 0) {
         let wordStr = (arr[word]).toString();
+        wordStr = upperFirstLetter(wordStr);
         console.log(wordStr);
     }
+}
+
+function upperFirstLetter(word) {
+        let char = word.charAt(0).toUpperCase();
+        word = word.slice(1);
+        word = char + word;
+        return(word);
 }
