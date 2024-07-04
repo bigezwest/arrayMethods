8c42f49ecaeaf40149e4ffba1d4bdfa1b81d9885
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 19:06:00 +0200

    refactor(sort): Refactor according to solution

--- tasks/sortDesc/sortDesc.js
+++ tasks/sortDesc/sortDesc.js
@@ -2,14 +2,6 @@
 
 let arr = [ 5, 2, 1, -10, 8];
 
-function compareNums(a, b) {
-    if (a > b) return 1;
-    if (a == b) return 0;
-    if (a < b) return -1;
-}
-
-
-arr.sort(compareNums)
-.reverse;
+arr.sort((a, b) => b - a);
 
 console.log(arr);
