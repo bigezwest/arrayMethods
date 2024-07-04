56c3ee48d3088afbebd921b084a9c2629c804c6d
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 10:54:56 +0200

    chore(comments, format): Add comments and grouping

--- unshift/unshift.js
+++ unshift/unshift.js
@@ -1,7 +1,10 @@
 "use strict";
 
+// Ex: 1 - unshift(string) -----------------------------------------------------
+console.group("Ex: 1 - unshift(string):")
 let names_arr = [ "Tom", "Dick", "Harry" ];
 console.log("Original Array: " + names_arr);
 let newString = "Fred";
 names_arr.unshift(newString);
 console.log("Unshifted Array: " + names_arr);
+console.groupEnd();
