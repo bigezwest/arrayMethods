2e2c907898901cb96ab9c0b91ed8976056c45dec
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 16:46:07 +0200

    feature(filterRange()): Add function
    * Add constants, arr, min, max
    * Add function filterRange(arr, min, max).  This uses a for in
    and checks if each item is within min, max.  If so, it adds it
    to newArr and then console.logs the new and original arrays.

--- tasks/filterRange/filterRange.js
+++ tasks/filterRange/filterRange.js
@@ -1,1 +1,17 @@
 "use strict";
+const arr = [ 5, 3, 8, 1 ];
+const min = 1;
+const max = 4;
+
+filterRange(arr, min, max);
+
+function filterRange(arr, a, b) {
+    let newArr = [];
+    for (let item in arr) {
+        if (arr[item] >= a && arr[item] <= b) {
+            newArr.push(arr[item]);
+        }
+    }
+    console.log("New Array: " + newArr);
+    console.log("Original Array: " + arr);
+}
