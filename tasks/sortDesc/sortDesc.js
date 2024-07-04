32001d73c1cb4317523e8a2e9c8462177d34542c
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 19:03:13 +0200

    feature(sort, reverse, compareNums()): Add funcs
    * Add compareNums() to compare as a number instead of a starting
    * Sort arr, then reverse

--- tasks/sortDesc/sortDesc.js
+++ tasks/sortDesc/sortDesc.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let arr = [ 5, 2, 1, -10, 8];
+
+function compareNums(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+}
+
+
+arr.sort(compareNums)
+.reverse;
+
+console.log(arr);
