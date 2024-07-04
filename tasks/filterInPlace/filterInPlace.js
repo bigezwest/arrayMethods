50c58409559aab8247523ef1e5a481c2200e7730
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 18:37:18 +0200

    feature(filterInPlace): Add function

--- tasks/filterInPlace/filterInPlace.js
+++ tasks/filterInPlace/filterInPlace.js
@@ -1,1 +1,18 @@
 "use strict";
+
+let arr = [ 5, 3, 8, 1 ];
+const min = 1;
+const max = 4;
+filterInPlace(arr, min, max);
+console.log(arr);
+
+function filterInPlace(arr, min, max) {
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i];
+
+        if (element < min || element > max) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+}
