efdd6d9fd27340a3113792df0cab5c109d14c1d1
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 10:38:57 +0200

    feature(shift()): Add example of shift
    * Add an array, display original
    * Perform shift on array
    * Display array after shift
    * Display shifted element

--- shift/shift.js
+++ shift/shift.js
@@ -1,1 +1,8 @@
 "use strict";
+
+let nums_arr = [ 1, 2, 3, 4, 5 ];
+console.log("Original Array: " + nums_arr);
+
+let shift_item = nums_arr.shift();
+console.log("After Shift: " + nums_arr);
+console.log("Shift Item: " + shift_item);
