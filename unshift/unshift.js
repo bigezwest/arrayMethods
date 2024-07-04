a2d05ccd482a3b6342d75717f3a4f162b13f6f54
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 10:51:58 +0200

    feature(unshift(string)): Add example
    * Add an example of unshift adding a string
    * Display before unshift and after

--- unshift/unshift.js
+++ unshift/unshift.js
@@ -1,1 +1,7 @@
 "use strict";
+
+let names_arr = [ "Tom", "Dick", "Harry" ];
+console.log("Original Array: " + names_arr);
+let newString = "Fred";
+names_arr.unshift(newString);
+console.log("Unshifted Array: " + names_arr);
