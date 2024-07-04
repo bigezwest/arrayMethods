2362b2afacb5bdb497f0e98f6928c5057a2ccb0b
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 10:58:57 +0200

    feature(unshift(...arr)): Add example
    * Show unshift an array to an array

--- unshift/unshift.js
+++ unshift/unshift.js
@@ -8,3 +8,12 @@ let newString = "Fred";
 names_arr.unshift(newString);
 console.log("Unshifted Array: " + names_arr);
 console.groupEnd();
+
+// Ex: 2 - unshift(...arr);
+console.group("unshift(...arr)")
+let names_arr2 = [ "Tom", "Dick", "Harry" ];
+console.log("Original Array: " + names_arr2);
+let names_new = [ "Fred", "Barney" ];
+names_arr2.unshift(...names_new);
+console.log("Unshifted Array: " + names_arr2);
+console.groupEnd();
