b23d33187cf811ac6641cc1c1772dd06d96e0e82
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 16:19:06 +0200

    refactor(camalize): Refactor according to solution
    * The solution provided used callbacks.  Everything was also in one
    function.  It is a cleaner block of code.

--- tasks/translate/translate.js
+++ tasks/translate/translate.js
@@ -1,23 +1,13 @@
 "use strict";
 
 let input = "change-this-string";
-camelize(input);
+console.log(camelize(input));
 
 function camelize(str) {
-    let arr = input.split("-");
-    for (let word = 0; word < arr.length; word++) {
-        if (word > 0) {
-            let wordStr = (arr[word]).toString();
-            wordStr = upperFirstLetter(wordStr);
-            arr[word] = wordStr;
-        }
-    }
-    console.log(arr.join(''));
-}
-
-function upperFirstLetter(word) {
-        let char = word.charAt(0).toUpperCase();
-        word = word.slice(1);
-        word = char + word;
-        return(word);
+    return str
+    .split("-")
+    .map(
+        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join('');
 }
