764d19d142a3b7865720e2756e6564ece1b65639
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-04 20:01:54 +0200

    feature(copySort): Add function copySorted(arr)
    * This function makes a copy with slice, and then sorts
    the array with defaults.

--- tasks/copySort/copySort.js
+++ tasks/copySort/copySort.js
@@ -1,1 +1,11 @@
 "use strict";
+
+let arr = [ "HTML", "JavaScript", "CSS" ];
+let sorted = copySorted(arr);
+
+console.log(sorted);
+
+function copySorted(arr) {
+    return arr.slice()
+    .sort();
+}
