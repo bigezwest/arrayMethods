389f1844b5571069bae3388c3caf73276379b09e
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-05 18:03:19 +0200

    feature(unique): Check for unique entriesß

--- tasks/unique/unique.js
+++ tasks/unique/unique.js
@@ -1,6 +1,15 @@
 "use strict";
 
-let strings = [
-    "Hare", "Krishna", "Hare", "Krishna",
-    "Krishna", "Krishna", "Hare", "Hare", ":-O"
-    ];
+const strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+
+function unique(arr) {
+    let result = [];
+
+    for (let string of arr) {
+        if (!result.includes(string)) {
+            result.push(string);
+        }
+    }
+    return result;
+}
+console.log(unique(strings));
