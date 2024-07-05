221261d45f9df2b5c800a80b650b24c3a71af25d
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-05 17:27:19 +0200

    feature(getAverageAge): Add function
    * Add function that uses reduce to get the user.age and add
    it to the accumulator for the length of users.)

--- tasks/getAverage/average.js
+++ tasks/getAverage/average.js
@@ -4,3 +4,11 @@ let pete = { name: "Pete", age: 30 };
 let mary = { name: "Mary", age: 29 };
 
 let arr = [ john, pete, mary ];
+
+const initialValue = 0;
+function getAverageAge(users) {
+    return users.reduce((accumulator, user) =>
+        accumulator + user.age, initialValue) / users.length;
+}
+
+console.log(getAverageAge(arr));
