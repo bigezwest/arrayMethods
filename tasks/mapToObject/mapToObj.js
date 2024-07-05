e39e5f591c35d587db117c6b64e0233c0cddf41f
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-05 10:18:24 +0200

    feature(data): Add starting data

--- tasks/mapToObject/mapToObj.js
+++ tasks/mapToObject/mapToObj.js
@@ -1,1 +1,7 @@
 "use strict";
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
