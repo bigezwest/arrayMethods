a52b5e44df57067ffca392bd7633792e8689caab
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-05 16:27:16 +0200

    feature(shuffle): Add shuffle functionality

--- tasks/shuffle/shuffle.js
+++ tasks/shuffle/shuffle.js
@@ -1,10 +1,28 @@
 "use strict";
 
-let arr = [1, 2, 3];
 
 const shuffle = (arr) => {
-    let arrCopy = arr.slice();
-    let length = arrCopy.length;
+    let min = 1;                                            // need min 1 for random
+    let arrCopy = arr.slice();                              // A copy to delete items from
+    let newArr = [];                                        // An arr to save to
+
+    while (newArr.length < arr.length) {
+        let randIndex = randNum(min, arrCopy.length + 1);   // Get random
+            newArr.push(arrCopy[randIndex - 1]);            // insert element from arrCopy, adjust to index
+            arrCopy.splice(randIndex -1, 1);                // Remove from arrCopy, adjust to index
+        }
+        return newArr;
 };
 
-shuffle(arr); // arr = [3, 2, 1]
+function randNum(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function run() {
+    let arr = [10, 20, 30, 40, 50];
+    for (let i = 0; i < 5; i++) {
+        console.log(shuffle(arr));
+    }
+}
+
+run();
