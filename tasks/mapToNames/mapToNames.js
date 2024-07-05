bf6ff131fb1677ffb3ce6b425d4186021f8adf0d
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-05 10:06:48 +0200

    feature(map users): Map users to user.name array

--- tasks/mapToNames/mapToNames.js
+++ tasks/mapToNames/mapToNames.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users =  [ john, pete, mary ];
+
+let names = users.map(user => {
+    return `${user.name}`;
+})
+
+console.log(names);
